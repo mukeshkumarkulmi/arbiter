27f355c905bc7e3e1e9874b2c3a12a418d9a8e56
Mukesh kumar kulmi <mukeshk.kulmi@impetus.co.in>
2017-05-31 18:13:28 +0530

    Resolved checkstyle error

--- src/main/java/com/etsy/arbiter/config/Global.java
+++ src/main/java/com/etsy/arbiter/config/Global.java
@@ -4,31 +4,35 @@ import java.util.List;
 import java.util.Map;
 
 public class Global {
-    
+
     private Map<String, List<String>> globalArgs;
     private Map<String, String> properties;
     private int configurationPosition;
-    
-    
+
     public Map<String, List<String>> getGlobalArgs() {
         return globalArgs;
     }
+
     public void setGlobalArgs(Map<String, List<String>> globalArgs) {
         this.globalArgs = globalArgs;
     }
+
     public Map<String, String> getProperties() {
         return properties;
     }
+
     public void setProperties(Map<String, String> properties) {
         this.properties = properties;
     }
+
     public int getConfigurationPosition() {
         return configurationPosition;
     }
+
     public void setConfigurationPosition(int configurationPosition) {
         this.configurationPosition = configurationPosition;
     }
-    
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -41,27 +45,34 @@ public class Global {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
+        if (this == obj) {
             return true;
-        if (obj == null)
+        }
+        if (obj == null) {
             return false;
-        if (getClass() != obj.getClass())
+        }
+        if (getClass() != obj.getClass()) {
             return false;
+        }
         Global other = (Global) obj;
-        if (configurationPosition != other.configurationPosition)
+        if (configurationPosition != other.configurationPosition) {
             return false;
+        }
         if (globalArgs == null) {
-            if (other.globalArgs != null)
+            if (other.globalArgs != null) {
                 return false;
-        } else if (!globalArgs.equals(other.globalArgs))
+            }
+        } else if (!globalArgs.equals(other.globalArgs)) {
             return false;
+        }
         if (properties == null) {
-            if (other.properties != null)
+            if (other.properties != null) {
                 return false;
-        } else if (!properties.equals(other.properties))
+            }
+        } else if (!properties.equals(other.properties)) {
             return false;
+        }
         return true;
     }
-    
-    
+
 }
